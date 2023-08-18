24066dbdfc8720bd40f48e327ee12d4507b9cfcd
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-08-17 18:36:30 -0700

    first commit

---
+++ ENUM.js
@@ -0,0 +1,28 @@
+
+class ENUM {
+    constructor() {
+        this.types = []
+    }
+
+    push( type ){
+        this.types.push(type)
+    }
+
+}
+
+class Type {
+
+    constructor( obj ){ // objType = { type: boolean } || { property }
+        this.type = obj.property;
+        this.value = false;
+
+        obj.value === undefined
+            ? this.value = false
+            : this.value = object.value
+   
+    }
+
+}
+
+module.exports = ENUM;
+module.exports = Type;
