59c1a6bcd42bafdf29889bca6cde9fa8d8dc2066
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-10-03 16:02:42 +0000

    Added tests to valueOf() method

---
+++ test/ENUM.chai.cjs
@@ -0,0 +1,82 @@
+const chai = require('chai'), expect = chai.expect;
+const ENUM = require('../ENUM.cjs')
+
+
+
+let counter = 1;
+
+describe(`ENUM.cjs`, () => {
+    describe(`Constructor`, () => {
+        it(`Test ${counter}: Initial Value`, () => {
+            const value = 'GENUS';
+            const en = new ENUM(value)
+
+            expect(en.GENUS).to.be.true;
+        })
+        counter++;  
+    })
+
+    describe(`Class Methods`, () => {
+        const value = 'genus';
+        const en = new ENUM(value)
+
+        en.setKey('type')
+        en.setKeys(['kInD','VARIETY'])
+
+        it(`Test ${counter}: ENUM.setKey(keyString)`, () => {
+            expect(en.TYPE).to.be.false;
+        })
+        counter++;
+
+        it(`Test ${counter}: ENUM.setKeys([strings])`, () => {
+            expect(en.KIND).to.be.false;
+            expect(en.VARIETY).to.be.false;
+        })
+        counter++;
+
+        it(`Test ${counter}: ENUM.selectKey(string)`, () => {
+            en.selectKey('kind')
+
+            expect(en.GENUS).to.be.false;
+            expect(en.TYPE).to.be.false;
+            expect(en.KIND).to.be.true;
+            expect(en.VARIETY).to.be.false;
+        })
+        counter++;
+
+        it(`Test ${counter}: ENUM.toString()`, () => {
+            const string = 
+                `ENUM {\n` +
+                `    {GENUS: false},\n` +
+                `    {TYPE: false},\n` +
+                `    {KIND: true},\n` +
+                `    {VARIETY: false}\n` +
+                `}`
+
+            expect(en.toString()).to.equal(string)
+        })
+        counter++;
+        
+        it(`Test ${counter}: ENUM.valueOf()`, () => {
+            expect(en.valueOf()).to.equal('KIND')
+        })
+        counter++;
+        
+    })
+
+})
+
+
+
+/**
+describe(`DESCRIPTION`, () => {
+    describe(`DESCRIPTION`, () => {
+        it(`SUMMARY`, () => {
+            // Expectations
+        })
+        counter++;
+        
+    })
+})
+
+*/
