064c06f182ec8e2e85babf9c1e63b922a92e3740
Replit user <>
2023-10-16 22:52:32 +0000

    Releasing changes

--- sh/gitRelease.sh
+++ sh/gitRelease.sh
@@ -5,12 +5,12 @@
 git commit . -m "Releasing changes"
 git push
 
-git checkout main
+git checkout Main
 git commit -m "Releasing changes"
 git merge Dev
 
 git checkout Release
 git commit -m "Releasing changes"
-git merge main
+git merge Main
 
 git checkout Dev
