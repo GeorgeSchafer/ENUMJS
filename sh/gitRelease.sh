84b20af7b34118a659ab5fdf5402896a0dacd94d
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-10-16 22:38:47 +0000

    Release Merge

---
+++ sh/gitRelease.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+git commit . -m "Releasing changes"
+git push
+
+git checkout main
+git commit -m "Releasing changes"
+git merge Dev
+
+git checkout Release
+git commit -m "Releasing changes"
+git merge main
+
+git checkout Dev
