e33604a412a52ecefe7cdbac47667feccbf3b456
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-10-19 04:20:42 +0000

    Updated toString to accept a fancy or unformatted boolean, prettified the files

--- ENUM.mjs
+++ ENUM.mjs
@@ -1,59 +1,55 @@
 // Import syntax
 // make changes to this file, then copy them over to the ENUMJS.cjs file
 
-
-
-
 export default class ENUM {
+  constructor(key) {
+    this[ensureUppercase(key)] = true;
+  }
 
-    constructor( key ) { 
-        this[ensureUppercase(key)] = true;
+  addKey(key) {
+    this[ensureUppercase(key)] = false;
+  }
+
+  addKeys(keyArray) {
+    keyArray.forEach((key) => {
+      this[ensureUppercase(key)] = false;
+    });
+  }
+
+  selectKey(key) {
+    const keys = Object.keys(this);
+    key = ensureUppercase(key);
+
+    if (this[key] != true && this[key] != false) {
+      throw new Error("InvalidKey Error: specified key is not present");
+    } else {
+      keys.forEach((element) => {
+        this[element] = false;
+      });
+
+      this[key] = true;
     }
+  }
 
-    setKey( key ){
-        this[ensureUppercase(key)] = false;
+  toString(fancy = false) {
+    const keyValuePairs = Object.keys(this).map(
+      (key) => `{${key}: ${this[key]}}`
+    );
+    if (fancy) {
+      return `ENUM {\n    ${keyValuePairs.join(",\n    ")}\n}`;
+    } else {
+      return `ENUM {${keyValuePairs.join(",")}}`;
     }
+  }
 
-    setKeys( keyArray ){
-        keyArray.forEach( (key) => {
-            this[ensureUppercase(key)] = false;
-        } )
-    }
-
-    selectKey( key ){
-        const keys = Object.keys(this)
-        key = ensureUppercase(key)
-
-        if(this[key] != true && this[key] != false){
-            throw new Error("InvalidKey Error: specified key is not present")
-        } else {
-            keys.forEach( (element) => {
-                this[element] = false;
-            } )
-
-            this[key] = true;
-        }
-    }
-
-    toString() {
-        const keyValuePairs = Object.keys(this).map(key => `{${key}: ${this[key]}}`);
-        return `ENUM {\n    ${keyValuePairs.join(',\n    ')}\n}`;
-    }
-
-    valueOf(){
-        let array = Object.keys(this);
-        for( let i = 0 ; i < array.length ; i++ ){
-            if(this[array[i]]){
-                return array[i];
-            }
-        }
-    }
-
+  valueOf() {
+    return Object.keys(this).find((key) => this[key]);
+  }
 }
 
-function ensureUppercase(key){
-    if(typeof key == 'string'){
-        key = key.toUpperCase()
-    }
-    return key;
+function ensureUppercase(key) {
+  if (typeof key == "string") {
+    key = key.toUpperCase();
+  }
+  return key;
 }
