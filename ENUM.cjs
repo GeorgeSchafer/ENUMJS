b7ffaa5114155950a315efd8f2afebca8f5c2b35
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-10-19 04:20:42 +0000

    Updated toString to accept a fancy or unformatted boolean, prettified the files

--- ENUM.cjs
+++ ENUM.cjs
@@ -1,75 +1,71 @@
 // require syntax
 // DO NOT work on this file directly. Make changes in the ENUM.mjs file, then copy them over
 
-
-
 module.exports = class ENUM {
+  constructor(key) {
+    this[filter(key)] = true;
+  }
 
-    constructor( key ) { 
-        this[filter(key)] = true;
+  setKey(key) {
+    this[filter(key)] = false;
+  }
+
+  setKeys(keyArray) {
+    keyArray.forEach((key) => {
+      this[filter(key)] = false;
+    });
+  }
+
+  selectKey(key) {
+    const keys = Object.keys(this);
+    key = filter(key);
+
+    if (this[key] != true && this[key] != false) {
+      throw new Error("InvalidKey Error: specified key is not present");
+    } else {
+      keys.forEach((element) => {
+        this[element] = false;
+      });
+
+      this[key] = true;
     }
+  }
 
-    setKey( key ){
-        this[filter(key)] = false;
-    }
+  toString() {
+    const keys = Object.keys(this);
+    let result = "";
+    let index = 0;
+    let length = keys.length;
 
-    setKeys( keyArray ){
-        keyArray.forEach( (key) => {
-            this[filter(key)] = false;
-        } )
-    }
-
-    selectKey( key ){
-        const keys = Object.keys(this)
-        key = filter(key)
-
-        if(this[key] != true && this[key] != false){
-            throw new Error("InvalidKey Error: specified key is not present")
-        } else {
-            keys.forEach( (element) => {
-                this[element] = false;
-            } )
-
-            this[key] = true;
-        }
-    }
-
-    toString(){
-        const keys = Object.keys(this)
-        let result = '';
-        let index = 0;
-        let length = keys.length;
-
-        keys.forEach((key, index) => {
-            if(index === 0){
-                result += `ENUM {\n    {${key}: ${this[key]}},\n`;
-            } else if(index >= keys.length - 1){
-                result += `    {${key}: ${this[key]}}\n}`;
-                return result;
-            } else {
-                result += `    {${key}: ${this[key]}},\n`;
-            }
-
-            index++;
-        })
-
+    keys.forEach((key, index) => {
+      if (index === 0) {
+        result += `ENUM {\n    {${key}: ${this[key]}},\n`;
+      } else if (index >= keys.length - 1) {
+        result += `    {${key}: ${this[key]}}\n}`;
         return result;
-    }
+      } else {
+        result += `    {${key}: ${this[key]}},\n`;
+      }
 
-    valueOf(){
-        let array = Object.keys(this);
-        for( let i = 0 ; i < array.length ; i++ ){
-            if(this[array[i]]){
-                return array[i];
-            }
-        }
-    }
+      index++;
+    });
 
-}
+    return result;
+  }
 
-function filter(key){
-    if(typeof key == 'string'){
-        key = key.toUpperCase()
+  valueOf() {
+    let array = Object.keys(this);
+    for (let i = 0; i < array.length; i++) {
+      if (this[array[i]]) {
+        return array[i];
+      }
     }
-    return key;
+  }
+};
+
+function filter(key) {
+  if (typeof key == "string") {
+    key = key.toUpperCase();
+  }
+  return key;
 }
--- test/ENUM.chai.mjs
+++ test/ENUM.chai.mjs
@@ -1,72 +1,69 @@
-import {expect, assert} from 'chai'
-import ENUM from '../ENUM.mjs'
-
-
+import { expect, assert } from "chai";
+import ENUM from "../ENUM.mjs";
 
 let counter = 1;
 
 describe(`ENUM.mjs`, () => {
-    describe(`Constructor`, () => {
-        it(`Test ${counter}: Initial Value`, () => {
-            const value = 'GENUS';
-            const en = new ENUM(value)
+  describe(`Constructor`, () => {
+    it(`Test ${counter}: Initial Value`, () => {
+      const value = "GENUS";
+      const en = new ENUM(value);
 
-            expect(en.GENUS).to.be.true;
-        })
-        counter++;  
-    })
+      expect(en.GENUS).to.be.true;
+    });
+    counter++;
+  });
 
-    describe(`Class Methods`, () => {
-        const value = 'genus';
-        const en = new ENUM(value)
+  describe(`Class Methods`, () => {
+    const value = "genus";
+    const en = new ENUM(value);
 
-        en.setKey('type')
-        en.setKeys(['kInD','VARIETY'])
+    en.addKey("type");
+    en.addKeys(["kInD", "VARIETY"]);
 
-        it(`Test ${counter}: ENUM.setKey(keyString)`, () => {
-            expect(en.TYPE).to.be.false;
-        })
-        counter++;
+    it(`Test ${counter}: ENUM.addKey(keyString)`, () => {
+      expect(en.TYPE).to.be.false;
+    });
+    counter++;
 
-        it(`Test ${counter}: ENUM.setKeys([strings])`, () => {
-            expect(en.KIND).to.be.false;
-            expect(en.VARIETY).to.be.false;
-        })
-        counter++;
+    it(`Test ${counter}: ENUM.addKeys([strings])`, () => {
+      expect(en.KIND).to.be.false;
+      expect(en.VARIETY).to.be.false;
+    });
+    counter++;
 
-        it(`Test ${counter}: ENUM.selectKey(string)`, () => {
-            en.selectKey('kind')
+    it(`Test ${counter}: ENUM.selectKey(string)`, () => {
+      en.selectKey("kind");
 
-            expect(en.GENUS).to.be.false;
-            expect(en.TYPE).to.be.false;
-            expect(en.KIND).to.be.true;
-            expect(en.VARIETY).to.be.false;
-        })
-        counter++;
+      expect(en.GENUS).to.be.false;
+      expect(en.TYPE).to.be.false;
+      expect(en.KIND).to.be.true;
+      expect(en.VARIETY).to.be.false;
+    });
+    counter++;
 
-        it(`Test ${counter}: ENUM.toString()`, () => {
-            const string = 
-                `ENUM {\n` +
-                `    {GENUS: false},\n` +
-                `    {TYPE: false},\n` +
-                `    {KIND: true},\n` +
-                `    {VARIETY: false}\n` +
-                `}`
+    it(`Test ${counter}: ENUM.toString()`, () => {
+      const string1 =
+        `ENUM {\n` +
+        `    {GENUS: false},\n` +
+        `    {TYPE: false},\n` +
+        `    {KIND: true},\n` +
+        `    {VARIETY: false}\n` +
+        `}`;
+      const string2 = `ENUM {{GENUS: false},{TYPE: false},{KIND: true},{VARIETY: false}}`;
 
-            expect(en.toString()).to.equal(string)
-        })
-        counter++;
-        
-        it(`Test ${counter}: ENUM.valueOf()`, () => {
-            expect(en.valueOf()).to.equal('KIND')
-        })
-        counter++;
-        
-    })
+      expect(en.toString(true)).to.equal(string1);
+      expect(en.toString()).to.equal(string2);
+      expect(en.toString(false)).to.equal(string2);
+    });
+    counter++;
 
-})
-
-
+    it(`Test ${counter}: ENUM.valueOf()`, () => {
+      expect(en.valueOf()).to.equal("KIND");
+    });
+    counter++;
+  });
+});
 
 /**
 describe(`DESCRIPTION`, () => {
