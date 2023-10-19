1270d47e2d5eba1474fae4b8619e84df4b6f89d3
GeorgeSchafer <77408854+GeorgeSchafer@users.noreply.github.com>
2023-10-19 03:58:14 +0000

    simplified toString() and ensured it passed unit testing

--- ENUM.mjs
+++ ENUM.mjs
@@ -35,35 +35,10 @@ export default class ENUM {
         }
     }
 
-    toString(){
-        const keyValuePairs = Object.keys(this).map({key:value} => {
-            `{${key}: ${value}}`
-        })
-        return `ENUM {\n    ${keyValuePairs.join(',\n    ')}\n}`
+    toString() {
+        const keyValuePairs = Object.keys(this).map(key => `{${key}: ${this[key]}}`);
+        return `ENUM {\n    ${keyValuePairs.join(',\n    ')}\n}`;
     }
-    /*
-    toString(){
-        const keys = Object.keys(this)
-        let result = '';
-        let index = 0;
-        let length = keys.length;
-
-        keys.forEach((key, index) => {
-            if(index === 0){
-                result += `ENUM {\n    {${key}: ${this[key]}},\n`;
-            } else if(index >= keys.length - 1){
-                result += `    {${key}: ${this[key]}}\n}`;
-                return result;
-            } else {
-                result += `    {${key}: ${this[key]}},\n`;
-            }
-            
-            index++;
-        })
-
-        return result;
-    }
-    */
 
     valueOf(){
         let array = Object.keys(this);
